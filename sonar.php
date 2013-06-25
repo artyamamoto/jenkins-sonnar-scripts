6e87a6e7bff4d68af06a3a1a1f6eccc500d4f616
root <root@ip-10-122-14-41.ap-northeast-1.compute.internal>
2013-06-25 12:24:05 +0900

    a

--- sonar.php
+++ sonar.php
@@ -3,24 +3,67 @@
 if (!empty($_SERVER["WORKSPACE"]) && !is_dir($_SERVER["WORKSPACE"])) {
 	die('$WORKSPACE not defined or not dir.'."\n");
 }
-main();
+Script::main();
 
-function main() {
-	$pmd = sprintf('%s/.sonar/build/logs/pmd.xml', $_SERVER["WORKSPACE"]);
-	$punit = sprintf('%s/.sonar/build/logs/punit.xml', $_SERVER["WORKSPACE"]);	
-	
-	if (! is_file($pmd)) 
-		die("$pmd not found.\n");
-	if (! ($pmd_xml = simplexml_load_file($pmd)))
-		die("failed to parse xml: $pmd\n");
-	
-	$violation = 0;
-	foreach($pmd_xml->file as $file) {
-		if ($cnt = count($file->violation))
-			$violation += $cnt;
+class Script {
+	function main() {
+		$_this = new self;
+		
+		echo "#=== jenkins sonar script start ============\n";
+		$_this->checkViolations();
+		$_this->checkUnitTests();
+		echo "#=== jenkins sonar script end   ============\n";
 	}
-	echo "$pmd\n";
-	printf("pmd violation: %d\n", $violation);
+	function checkUnitTests() {
+		echo "#=== phpunit.xml =======================\n";
+		
+		$punit = sprintf('%s/.sonar/build/logs/phpunit.xml', $_SERVER["WORKSPACE"]);	
+		if (!is_file($punit))
+			die("$punit not found. \n");
+		if (!($punit = simplexml_load_file($punit)) )
+			die("failed to parse xml: $punit\n");
+		
+		$failures = array();
+		$errors = array();
+		foreach($punit->testsuite as $suite) {
+			foreach($suite->testsuite as $_suite) {
+				foreach($_suite->testcase as $case) {
+					if (!empty($case->failure)) 
+						$failures[] = (string)$case->failure;
+					if (!empty($case->error))
+						$errors[] = (string)$case->error;
+				}
+			}
+		}
+		if (!empty($failures)) {
+			printf("#=== Failures -- %d found. ===\n", count($failures));
+			foreach($failures as $failure) 
+				printf("---\n%s\n---\n", trim($failure));
+		}
+		if (!empty($errors)) {
+			printf("#=== Errors -- %d found. ===\n", count($errors));
+			foreach($errors as $error) 
+				printf("---\n%s\n---\n", trim($error));
+		}
+	}
+	function checkViolations() {
+		echo "#=== pmd.xml ===========================\n";
+		
+		$pmd = sprintf('%s/.sonar/build/logs/pmd.xml', $_SERVER["WORKSPACE"]);
+		
+		if (! is_file($pmd)) 
+			die("$pmd not found.\n");
+		if (! ($pmd_xml = simplexml_load_file($pmd)))
+			die("failed to parse xml: $pmd\n");
+		
+		$violation = 0;
+		foreach($pmd_xml->file as $file) {
+			if ($cnt = count($file->violation))
+				$violation += $cnt;
+		}
+		echo "$pmd\n";
+		printf("pmd violation: %d\n", $violation);
+	}
 }
 
 

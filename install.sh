85e5b69be22853095fcba58d52d61b6bea478b22
artyamamoto <yamamoto.k@art-bank.co.jp>
2013-06-26 11:54:13 +0900

    Makefile -> install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-SONAR_RESULT_HOME = /usr/share/sonar-result
+BASEDIR=$(dirname "$0")
+SONAR_RESULT_HOME=/usr/share/sonar-result
+
 if [ "root" != `whoami` ] ; then
-	@echo "root user is required."
+	echo "root user is required."
 else
-	mkdir -p $(SONAR_RESULT_HOME)
-	cp -p ./trigger-sonar-runner $(SONAR_RESULT_HOME)/
-	cp -p ./sonar-result.php $(SONAR_RESULT_HOME)/
+	mkdir -p "${SONAR_RESULT_HOME}"
+	cp -p ./trigger-sonar-runner "${SONAR_RESULT_HOME}/"
+	cp -p ./sonar-result.php "${SONAR_RESULT_HOME}/"
 	if grep "^tomcat" /etc/passwd ; then
-		chown -R tomcat:tomcat $(SONAR_RESULT_HOME)/
+		chown -R tomcat:tomcat "${SONAR_RESULT_HOME}/"
 	fi
-	chmod ugo+x $(SONAR_RESULT_HOME)/trigger-sonar-runner
+	chmod ugo+x "${SONAR_RESULT_HOME}/trigger-sonar-runner"
+
+	echo "installed ${SONAR_RESULT_HOME}/trigger-sonar-runner"
 fi      
 
 
